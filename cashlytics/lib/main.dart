82f10b909554a65cf2646a31f92ab41b5229556b
WenHao1223 <wenhaojshs@gmail.com>
2025-12-20 02:17:39 +0800

    feat: setup supabase deep link

--- cashlytics/lib/main.dart
+++ cashlytics/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // import the other pages
 import 'pages/homepage.dart';
@@ -11,7 +14,29 @@ import 'pages/forgetpassword.dart';
 import 'pages/otpverification.dart';
 import 'pages/resetpassword.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables based on the build mode
+  // debug mode: .env.local
+  // release mode: .env.production
+  if (const bool.fromEnvironment('dart.vm.product')) {
+    await dotenv.load(fileName: ".env.production");
+  } else {
+    await dotenv.load(fileName: ".env.local");
+  }
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: dotenv.env['PUBLIC_SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['PUBLIC_SUPABASE_ANON_KEY'] ?? '',
+  );
+  final supabase = Supabase.instance.client;
+
+  // Use path URL strategy for web
+  usePathUrlStrategy();
+
+
   runApp(const MyApp());
 }
 
