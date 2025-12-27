7e16eda02401aa6aa8ce52ba4a3af6135f5b53fb
Oong Xian Ming <oongxianming04@gmail.com>
2025-12-27 17:47:24 +0800

    refactor: replace receipt repository and insert receipt only after expense save

--- mobile/lib/domain/repositories/receipt_repository.dart
+++ mobile/lib/domain/repositories/receipt_repository.dart
@@ -2,4 +2,13 @@ import 'package:cashlytics/domain/entities/receipt.dart';
 
 abstract class ReceiptRepository {
   Future<Receipt> upsertReceipt(Receipt receipt);
+
+  Future<String> uploadReceiptImage({
+    required Object imageSource,
+    required String receiptId,
+  });
+
+  Future<Receipt?> getReceiptById(String receiptId);
+
+  Future<String> getSignedReceiptUrl(String storagePath);
 }
