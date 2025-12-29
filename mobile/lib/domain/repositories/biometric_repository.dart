0a3a18966cd79eed285c18a43ba4e5c5f60a5b74
WenHao1223 <wenhaojshs@gmail.com>
2025-12-29 20:16:35 +0800

    feat: remove biometrics

--- mobile/lib/domain/repositories/biometric_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cashlytics/domain/entities/biometric.dart';
-
-abstract class BiometricRepository {
-  Future<Biometric> upsertBiometric(Biometric biometric);
-  Future<void> deleteBiometric(String biometricId);
-}
